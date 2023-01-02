c2bdc6ea1f90f275675dd6c227c47b9d56999c3d
GibadullinTagir <Gibadullin.t.r@gmail.com>
2023-01-02 19:00:51 +0300

    ferst

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,32 @@
 import '../scss/style.scss'
 
 console.log('Works!')
+
+
+// const roundBtnCall = document.createElement('button');
+// const imgCall = document.createElement('img');
+// imgCall.src = '../img/btn/call.svg';
+
+// const headerNav = document.querySelector('.header__nav');
+
+// roundBtnCall.classList.add('component_round-btn')
+
+// roundBtnCall.append(imgCall);
+// headerNav.prepend(roundBtnCall);
+
+
+
+
+// console.log(typeof(headrNavMenu))
+
+
+let a = 1;
+let b = "1";
+a==b
+true == 1 
+function res (a,b){
+    if(a == b){
+        console.log('==')
+    }
+}
+res(true,1);
---
+++ src/scss/page/_page__header.js
@@ -0,0 +1,5 @@
+// const roundBtnCall = document.createElement('button');
+// const imgCall = document.createElement('img');
+// imgCall.src = '../img/btn/call.svg';
+
+// console.log('fass')
